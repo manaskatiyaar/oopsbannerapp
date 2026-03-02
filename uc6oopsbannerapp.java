2a70d41650646ef33edb8370a1af4499cc648095
manaskatiyaar <manas.desk@gmail.com>
2026-03-02 11:06:02 +0530

    sdjhfgs

---
+++ uc6oopsbannerapp.java
@@ -0,0 +1,22 @@
+public class uc6oopsbannerapp {
+        public static String[] getOOPSpattern() {
+        return new String[]{
+
+                "  ***    ***    *****   ***** ",
+                " *   *  *   *   *    *  *     ",
+                "*     **     *  *    *  *     ",
+                "*     **     *  *****   ***** ",
+                "*     **     *  *           * ",
+                " *   *  *   *   *           * ",
+                "  ***    ***    *       ***** "
+        };
+    }
+
+    public static void main(String[] args) {
+        String[] str=getOOPSpattern();
+       for(int i=0;i<str.length;i++){
+           System.out.println(str[i]);
+       }
+    }
+    
+}
