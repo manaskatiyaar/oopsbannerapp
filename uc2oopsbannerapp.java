591cd3a50e82444b98410bdb4f6234d654fd092d
manaskatiyaar <manas.desk@gmail.com>
2026-03-02 10:46:55 +0530

    Added UC2 file

---
+++ uc2oopsbannerapp.java
@@ -0,0 +1,13 @@
+public class uc2oopsbannerapp {
+      public static void main(String[] args) {
+        System.out.println("  *****   " + "  *****   " + "  ******  " + "   *****  ");
+        System.out.println(" **   **  " + " **   **  " + " **   **  " + "  **      ");
+        System.out.println(" **   **  " + " **   **  " + " **   **  " + "   ***    ");
+        System.out.println(" **   **  " + " **   **  " + " ******   " + "      **  ");
+        System.out.println(" **   **  " + " **   **  " + " **       " + "      **  ");
+        System.out.println(" **   **  " + " **   **  " + " **       " + " **   **  ");
+        System.out.println("  *****   " + "  *****   " + " **        " + "  *****   ");
+
+    }
+    
+}
